844707f205e842474504c3659d563614ad7d8a06
Piotr Witaszek <p.a.witaszek@gmail.com>
2022-06-24 15:13:39 +0200

    Branch

--- learning-git-task.py
+++ learning-git-task.py
@@ -1,3 +1,6 @@
+from sre_constants import BRANCH
+
+
 lista_zakupów= {
     "warzywniak": ["marchew", "seler", "rukola"],
     "piekarnia": ["bułki", "chleb", "pączek"]
@@ -8,3 +11,4 @@ for keys in lista_zakupów:
 print("W sumię kupuję", sum([len(x) for x in lista_zakupów.values()]), "produktów.")
 print(commit test)
 print(commit test2)
+print(commit branch)
