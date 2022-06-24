c52d616608ac23d61f5d6898968828c0c2c90f6c
Piotr Witaszek <p.a.witaszek@gmail.com>
2022-06-24 12:33:23 +0200

    Add capitalization

--- learning-git-task.py
+++ learning-git-task.py
@@ -2,3 +2,6 @@ lista_zakupów= {
     "warzywniak": ["marchew", "seler", "rukola"],
     "piekarnia": ["bułki", "chleb", "pączek"]
 }
+print(lista_zakupów)
+for keys in lista_zakupów:
+    print("Idę do %s i kupuję tam: %s" % (keys.capitalize(),  [x.capitalize() for x in lista_zakupów[keys]]))
