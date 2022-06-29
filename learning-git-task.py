9dc5acd216c8cd14525aaea9d372118ca6ca60eb
Piotr Witaszek <p.a.witaszek@gmail.com>
2022-06-29 20:17:55 +0200

    Cleaning

--- learning-git-task.py
+++ learning-git-task.py
@@ -1,6 +1,3 @@
-from sre_constants import BRANCH
-
-
 lista_zakupów= {
     "warzywniak": ["marchew", "seler", "rukola"],
     "piekarnia": ["bułki", "chleb", "pączek"]
@@ -9,6 +6,6 @@ print(lista_zakupów)
 for keys in lista_zakupów:
     print("Idę do %s i kupuję tam: %s" % (keys.capitalize(),  [x.capitalize() for x in lista_zakupów[keys]]))
 print("W sumię kupuję", sum([len(x) for x in lista_zakupów.values()]), "produktów.")
-print(commit test)
-print(commit test2)
-print(commit branch)
+print("commit test")
+print("commit test2")
+print("commit branch")
